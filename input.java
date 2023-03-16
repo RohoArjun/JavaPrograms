e73ceba3c46b42860fe66d5f314bc2b2bde122fe
Arjun <arjunnarale9998@gmail.com>
2023-03-16 15:21:42 +0530

    new files commits

---
+++ input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String args[]) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter two number");
+        int a1 = a.nextInt();
+        int b = a.nextInt();
+        System.out.println("the value of a is " + a1 + "the value of b is " + b);
+
+    }
+
+}
