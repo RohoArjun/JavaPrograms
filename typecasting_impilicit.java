c2cd0a9d346789a56146bbcbfc7db1f650fd97d7
Arjun <arjunnarale9998@gmail.com>
2023-03-16 15:46:49 +0530

    latest file

---
+++ typecasting_impilicit.java
@@ -0,0 +1,7 @@
+public class typecasting_impilicit {
+    public static void main(String args[]) {
+        double a = 10.8989; // 8byte
+        int b = (int) a; // 4byte
+        System.out.println(b);
+    }
+}
