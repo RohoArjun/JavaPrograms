ba58447c3fd360c63938de9e8bf8cb0ab609c2e8
Arjun <arjunnarale9998@gmail.com>
2023-03-21 10:43:41 +0530

    todays new files

---
+++ for.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class newclass {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
