e12f022924c2c8ac0829963b5c6b44281f97aac7
Arjun <arjunnarale9998@gmail.com>
2023-03-23 10:30:10 +0530

    todays files

---
+++ sumNatural.java
@@ -0,0 +1,11 @@
+public class sumNatural {
+    public static void main(String[] args) {
+        int n = 20;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+
+        }
+        System.out.println(sum);
+    }
+}
